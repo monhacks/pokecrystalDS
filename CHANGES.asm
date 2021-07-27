fc648ddc761a3f3b42075e124e86c780890eebb7
DuckSalmon <seanbb.3s@gmail.com>
2021-07-28 01:22:17 +0200

    1

--- CHANGES.asm
+++ CHANGES.asm
@@ -1,45 +1,41 @@
+
+WIP
+check loading Kanto badges 
+fix pokemon encounter (night and evening not working)
+
+;;;;;;DONE:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;Tiles 
 -add battletower outside (NOW WITH RUINS OF ALPHA TILES)
 -add snowy tiles (curr unused)
--edit johto modern 
--edit johto 
+-add Tiles attributes X-Y Flip and Priority      https://github.com/pret/pokecrystal/wiki/Allow-tiles-to-have-different-attributes-in-different-blocks-(including-X-and-Y-flip)
+-Expand Tiles to 255
 
 ;Maps
-
-Town
 -edit CHERRYGROOVE  (beach, flowers and events edited) ;to do sea path 
 -edit Violet		(bridge and lantern) lantern from polished
 -edit ecruteak		(particles and roof colored trees)
 -edit Azalea 		(Roofs, wood ) wood stack from polished
 -edit Goldenrod     (Floor now gold, Fenches from Beta,street lamps, no more smurfs's little houses.
--edit Olivive       (new lighthouse and now looks like a port city) Now use johto-modern
+-edit Olivine       (new lighthouse and now looks like a port city) Now use johto-modern
 -edit Cianwood		(HG/SS style Now) now use johto-modern
+- edit Pallet
+- edit viridian
+;;all kanto is huge WIP
 
-Routes
--edit route 30 31 32 
-
-Others
-
--Ruin of alph (polished roof)
-
-
+;Menu
+-improve Show the tops of leaders' heads on the trainer card      https://github.com/pret/pokecrystal/wiki/Show-the-tops-of-leaders-heads-on-the-trainer-card
+-add kanto badges on player trainer card
+-Rearranged Pokegear icons and removed the clock icon (now a sub menu) for an extra space for a Music player cart
+-add unuque and color party menu icons https://github.com/pret/pokecrystal/wiki/Color-party-menu-icons-by-species
 
 ;misc
 -FIX blockset 
 -improve outdoor system    https://github.com/pret/pokecrystal/wiki/Improve-the-outdoor-sprite-system
 -add unique colors for each pokeball    https://github.com/pret/pokecrystal/wiki/Use-unique-colors-for-each-thrown-Pok%C3%A9-Ball
 -remove redunant variable sprites    https://github.com/pret/pokecrystal/wiki/Improve-the-outdoor-sprite-system#5-remove-the-now-redundant-variable-sprites
--improve Show the tops of leaders' heads on the trainer card      https://github.com/pret/pokecrystal/wiki/Show-the-tops-of-leaders-heads-on-the-trainer-card
 -remove redunant move grammar https://github.com/pret/pokecrystal/wiki/Remove-the-redundant-move-grammar-table
--Remuove "/" from the stat screen and from player card.
--add Tiles attributes X-Y Flip and Priority      https://github.com/pret/pokecrystal/wiki/Allow-tiles-to-have-different-attributes-in-different-blocks-(including-X-and-Y-flip)
-- add color party menu icons https://github.com/pret/pokecrystal/wiki/Color-party-menu-icons-by-species
-- Expand Tiles to 255
-- Rearranged Pokegear icons and removed the clock icon (now a sub menu) for an extra space for a Music player cart
 
-;Graphic
-- Add pokemon icons		https://github.com/pret/pokecrystal/wiki/Add-a-new-party-menu-icon
-- Add Roof Azalea
 
 ;Sprite
 - add miltank sprite icon for route 39
@@ -48,23 +44,21 @@ Others
 
 
 
-TO DO:
+;TO DO:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;NOW;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+fix gate kanto connections
+Fix all Kanto maps
 add violet Garden 11*9 North connection
 ADD a way to become the owner of the house in violet garden, unlocking the flypoint and adding a system (like the decoration system) to customize this house.
 fix violet route31 connection
-;change entire olivine map group to modern
 fix route 32 bonds and roofs adding cherry path (cherry will be connected to violet map group) find a way to change roofs without warps. for violet and cherry paths.
 move ruin of alfa in a new map group and give blue roof palette
 fix and check evening palette and other bg palette
 Check fountain anim
 edit mahogany roof pal to Red/brown
-https://github.com/pret/pokecrystal/wiki/Sideways-stairs-with-diagonal-movement
-fly point cherrygroove (fix)
 add restore egg ticket and odd eggs in PCC
-add kanto badges on player trainer card
+restore GS celebi event
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;WIP or IDEAS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 add quick level up system in your home, similar to the breeders but only for single pokemon. no eggs but quicker esp x4.
@@ -73,7 +67,6 @@ restore your pkm in your bed.
 add another buyable house in kanto.
 change how map sign sprite is loaded (no more big sign that cover half screen)
 idea? ;change how text box are loaded adding the sx and dx frame tiles (instead of a single centered one)
-restore GS celebi event
 restore pokecomm center and use it for post game (adding virtual battle against NPC and trainer from different generation or for unlock virtual journey/events and facing npc like giovanni or for make test rooms, alternate gyms)
 check headbutt tree pokemon encounters and add violet city encounters
 make Mareep, Remoraid, vulpix, mankey, girafarig catchable 
